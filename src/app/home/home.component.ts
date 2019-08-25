462b1bc9807566f6db820d6e1d51c3edb622d8e7
next-juanantoniohernandez <juanantoniohb93@gmail.com>
2019-08-25 17:42:55 +0200

    Modified Home Page, now description is clickable and it expands and hides

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -5,11 +5,22 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
+
 export class HomeComponent implements OnInit {
 
   constructor() { }
+  expand = [false, false, false, false];
 
   ngOnInit() {
   }
 
+  showDiv(id) {
+    this.expand[id] = !this.expand[id];
+    const elementId = 'panel-' + id;
+    if (this.expand[id] === true) {
+      document.getElementById(elementId).style.maxHeight = '1000px';
+    } else {
+      document.getElementById(elementId).style.maxHeight = '';
+    }
+  }
 }
