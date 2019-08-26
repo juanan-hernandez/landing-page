68fc90ce3418f63f66469ccedca3b52bc4edbcef
next-juanantoniohernandez <juanantoniohb93@gmail.com>
2019-08-26 18:54:52 +0200

    adapted CV experiences to make it responsive and adjust its content

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,7 +9,7 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
 
   constructor() { }
-  expand = [false, false, false, false];
+  expand = [false, false, false, false, false];
 
   ngOnInit() {
   }
