d2969d2b78715d245214a97726aa647081be7049
next-juanantoniohernandez <juanantoniohb93@gmail.com>
2019-09-09 19:23:04 +0200

    changes within the contact page and internationalization i18n

--- src/app/contact/contact.component.spec.ts
+++ src/app/contact/contact.component.spec.ts
@@ -1,3 +1,4 @@
+
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ContactComponent } from './contact.component';
@@ -8,9 +9,9 @@ describe('ContactComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ContactComponent ]
+      declarations: [ContactComponent]
     })
-    .compileComponents();
+        .compileComponents();
   }));
 
   beforeEach(() => {
--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Injectable } from '@angular/core';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.scss']
 })
+
+@Injectable({
+  providedIn:  'root'
+})
+
 export class ContactComponent implements OnInit {
-
-  constructor() { }
-
+  name: string;
+  email: string;
+  subject: string;
+  message: string;
   ngOnInit() {
   }
-
+  /**
+   * Process the form we have. Send to whatever backend
+   * Only alerting for now
+   */
+  processForm() {
+    const allInfo = `My name is ${this.name}. The subject is ${this.subject}. My email is ${this.email}. My message is ${this.message}`;
+    alert('hola');
+  }
 }
