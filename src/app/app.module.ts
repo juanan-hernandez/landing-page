53b4690075598bcf5a070219ce11b33ae91a0128
next-juanantoniohernandez <juanantoniohb93@gmail.com>
2019-09-15 17:48:49 +0200

    well referenced Material Design along with Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import {NgModule, NO_ERRORS_SCHEMA} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
@@ -27,6 +27,7 @@ import { ContactComponent } from './contact/contact.component';
     AppRoutingModule,
     MDBBootstrapModule.forRoot()
   ],
+  schemas:  [ NO_ERRORS_SCHEMA ],
   providers: [],
   bootstrap: [AppComponent]
 })
