ab825ce1a372c523a90751b983aebb6cfa1b1a35
juanan-hernandez <juanantoniohb93@gmail.com>
2019-12-21 15:45:07 +0100

    Updated Language Tabs. Now it belongs to the nav bar

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, LOCALE_ID, Inject, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-nav-bar',
@@ -6,8 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav-bar.component.scss']
 })
 export class NavBarComponent implements OnInit {
+  languageList = [
+    { code: 'en', label: 'English' },
+    { code: 'es', label: 'Español' }  ];
 
-  constructor() { }
+  constructor(@Inject(LOCALE_ID) protected localeId: string) {
+  }
 
   ngOnInit() {
   }
